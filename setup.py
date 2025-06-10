705b70249682472db082b5e1efc6e69d08834fd7
zilongshen <luzipeng01@sina.com>
2025-06-10 17:21:57 +0800

    find package

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     cmdclass=versioneer.get_cmdclass(),
     # 补充项目的其他必要信息，如name、packages等
     name='hello-world-pythonbill01',
-    packages=['hello_world'],
+    packages=find_packages(),
     author='zilongshen',
     author_email='luzipeng01@sina.com',
     description='A simple Hello World Python application.',
