7939f58ba83ca1ffec9e0b87b72d153afb973f17
zilongshen <luzipeng01@sina.com>
2025-06-10 17:11:00 +0800

    update to use setup.py to build wheel and tarball

--- setup.py
+++ setup.py
@@ -7,4 +7,15 @@ setup(
     # 补充项目的其他必要信息，如name、packages等
     name='hello-world-pythonbill01',
     packages=['hello_world'],
+    author='zilongshen',
+    author_email='luzipeng01@sina.com',
+    description='A simple Hello World Python application.',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    license='MIT',
+    # 此处省略其他参数，可按需补充
+    version='',  # 版本号由versioneer生成，此处先空着
+    cmdclass={},  # 若有自定义命令类，可在此补充
 )
